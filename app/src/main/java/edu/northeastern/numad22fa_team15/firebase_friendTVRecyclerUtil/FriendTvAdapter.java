6ca2745fa318e0ac873f79f30c661c8f919df0ed
Xiaojin Cai <xcai0511@gmail.com>
2022-11-01 18:31:16 -0400

    recycler view for friend list

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/firebase_friendTVRecyclerUtil/FriendTvAdapter.java
@@ -0,0 +1,29 @@
+package edu.northeastern.numad22fa_team15.firebase_friendTVRecyclerUtil;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.northeastern.numad22fa_team15.R;
+import edu.northeastern.numad22fa_team15.movieTvRecyclerUtil.MovieTvViewHolder;
+
+public class FriendTvAdapter extends RecyclerView.Adapter<FriendTvViewHolder> {
+    @NonNull
+    @Override
+    public FriendTvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendTvViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/firebase_friendTVRecyclerUtil/FriendTvViewHolder.java
@@ -0,0 +1,19 @@
+package edu.northeastern.numad22fa_team15.firebase_friendTVRecyclerUtil;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class FriendTvViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView itemFriend;
+
+    public FriendTvViewHolder(@NonNull View itemView) {
+        super(itemView);
+        itemFriend = itemView.findViewById(R.id.friend_nameTV);
+    }
+}
