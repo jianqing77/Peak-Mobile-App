5f4c9863e0dae15d6d59a5037ba993e04e35221d
xinyu-hou <61299181+xinyu-hou@users.noreply.github.com>
2022-11-03 13:52:18 -0400

    Add Friend Feature Implemented

--- app/src/main/java/edu/northeastern/numad22fa_team15/FirebaseFriendListActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/FirebaseFriendListActivity.java
@@ -1,10 +1,13 @@
 package edu.northeastern.numad22fa_team15;
 
+import static edu.northeastern.numad22fa_team15.utils.commonUtils.displayMessageInSnackbar;
+import static edu.northeastern.numad22fa_team15.utils.firebaseUtils.checkUserExistenceInFirebase;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +18,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,6 +41,7 @@ public class FirebaseFriendListActivity extends AppCompatActivity {
     private TextView numOfStickersSentTextView;
 
     private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference usersDatabaseReference;
     private DatabaseReference userDatabaseReference;
     private DatabaseReference numOfStickersDatabaseReference;
     private DatabaseReference friendsDatabaseReference;
@@ -61,8 +67,9 @@ public class FirebaseFriendListActivity extends AppCompatActivity {
         // Get the instance of the Firebase database.
         firebaseDatabase = FirebaseDatabase.getInstance();
         Log.v(TAG, "Firebase Database " + firebaseDatabase.toString());
+        usersDatabaseReference = firebaseDatabase.getReference().child("users");
         // Get the "user" reference for our database.
-        userDatabaseReference = firebaseDatabase.getReference().child("users").child(username);
+        userDatabaseReference = usersDatabaseReference.child(username);
 
         friendResults = new ArrayList<Friend>();
         friendsRecyclerView = findViewById(R.id.friends_recycler_view);
@@ -93,6 +100,7 @@ public class FirebaseFriendListActivity extends AppCompatActivity {
                     return;
                 }
                 Map<String, String> friendsMap = (Map<String, String>) friendsValue;
+                // Update friends RecyclerView
                 friendResults.clear();
                 for (Map.Entry<String, String> entry : friendsMap.entrySet()) {
                     String friendUsername = entry.getKey();
@@ -154,25 +162,92 @@ public class FirebaseFriendListActivity extends AppCompatActivity {
         AlertDialog.Builder b = new AlertDialog.Builder(this);
         b.setCancelable(false);
         b.setTitle("Add a Friend");
-        b.setView(R.layout.dialog_add_friend);
+        View dialogView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dialog_add_friend, null);
+        b.setView(dialogView);
 
         // Set up buttons
         b.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 // TO DO:
-                // (0) Check if the given username is the same as the current username
-                // (0a) [YES] Display message in Snackbar "Cannot add yourself as a friend."
-                // (0b) [NO] Go to step (1)
-                // (1) Check if the given username exists in the firebase realtime database
-                // (1a) [YES] Go to step (2)
-                // (1b) [NO] Display message in Snackbar "User does not exist."
+                // (1) Check if the given username is the same as the current username
+                // (1a) [YES] Display message in Snackbar "Cannot add yourself as a friend."
+                // (1b) [NO] Go to step (2)
+                EditText friendUsernameEditText = dialogView.findViewById(R.id.friend_username_edit_text);
+                String friendUsername = friendUsernameEditText.getText().toString();
+                Log.v(TAG, String.format("Trying to add user %s as friend.", friendUsername));
+                Log.v(TAG, "(1) Check if friend's username is the same as the current username.");
+                String currentUsername = currentUserTextView.getText().toString();
+                if (friendUsername.equals(currentUsername)) {
+                    String errorMessage = "Cannot add yourself as a friend.";
+                    Log.v(TAG, errorMessage);
+                    displayMessageInSnackbar(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_SHORT);
+                    return;
+                }
                 // (2) Check if the current user already has this person as a friend
                 // (2a) [YES] Display message in Snackbar "You already have this user as a friend."
                 // (2b) [NO] Go to step (3)
-                // (3) Try to add the given username in current user's friends list
-                // (3a) [YES] Display message in Snackbar "Friend added successfully."
-                // (3b) [NO] Display error message in Snackbar "Failed to add user as friend."
+                Log.v(TAG, "(2) Check if current user already has this user as a friend.");
+                boolean existence = usernameExistenceInFriendsList(friendUsername);
+                if (existence) {
+                    String errorMessage = "You already have this user as a friend.";
+                    Log.v(TAG, errorMessage);
+                    displayMessageInSnackbar(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_SHORT);
+                    return;
+                }
+                // (3) Check if the given username exists in the firebase realtime database
+                // (3a) [YES] Go to step (4)
+                // (3b) [NO] Display message in Snackbar "User does not exist."
+
+                // (4) Try to add the given username in current user's friends list
+                // (4a) [YES] Display message in Snackbar "Friend added successfully."
+                // (4b) [NO] Display error message in Snackbar "Failed to add user as friend."
+                Log.v(TAG, "(3) Check if friend's username exists in the database.");
+                Task<DataSnapshot> task1 = usersDatabaseReference.get();
+
+                task1.addOnCompleteListener(task -> {
+                    if(!task1.isSuccessful()){
+                        // A more specific message should be "Failed to retrieve users' info from firebase."
+                        String errorMessage = "Add friend feature is currently unavailable.";
+                        Log.v(TAG, errorMessage);
+                        displayMessageInSnackbar(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_SHORT);
+                    } else {
+                        // Check if the given username exists in the firebase realtime database
+                        boolean existenceResult = checkUserExistenceInFirebase(TAG, friendUsername, task1);
+                        // If yes, step (4)
+                        if (existenceResult) {
+                            Task<Void> task2 = friendsDatabaseReference.child(friendUsername).setValue(friendUsername);
+
+                            task2.addOnCompleteListener(anotherTask -> {
+                                if(!task2.isSuccessful()){
+                                    String errorMessage = String.format("Failed to add friend %s.", friendUsername);
+                                    Log.v(TAG, errorMessage);
+                                    displayMessageInSnackbar(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_SHORT);
+                                } else {
+                                    // Add yourself as a friend the that user's friends list
+                                    Task<Void> task3 = usersDatabaseReference.child(friendUsername).child("friends").child(currentUsername).setValue(currentUsername);
+
+                                    task3.addOnCompleteListener(finalTask -> {
+                                        if(!task3.isSuccessful()){
+                                            String errorMessage = String.format("Failed to add friend %s.", friendUsername);
+                                            friendsDatabaseReference.child(friendUsername).removeValue();
+                                            Log.v(TAG, errorMessage);
+                                            displayMessageInSnackbar(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_SHORT);
+                                        } else {
+                                            String message = String.format("Friend %s added.", friendUsername);
+                                            Log.v(TAG, message);
+                                            displayMessageInSnackbar(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT);
+                                        }
+                                    });
+                                }
+                            });
+                        } else { // If no, display message in a Snackbar.
+                            String errorMessage = "User does not exist.";
+                            Log.v(TAG, errorMessage);
+                            displayMessageInSnackbar(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_SHORT);
+                        }
+                    }
+                });
             }
         });
         b.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -184,6 +259,16 @@ public class FirebaseFriendListActivity extends AppCompatActivity {
 
         AlertDialog alert = b.create();
         alert.show();
+    }
+
+    private boolean usernameExistenceInFriendsList(String friendUsername) {
+        for (Friend existingFriend : this.friendResults) {
+            String existingFriendUsername = existingFriend.getUsername();
+            if (friendUsername.equals(existingFriendUsername)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
