4298e53187a0212f7291d24fcbb0cbb214e54137
xinyu-hou <61299181+xinyu-hou@users.noreply.github.com>
2022-11-05 10:43:41 -0400

    Handle Unavailable Sticker Edge Case in FirebaseStickerHistoryActivity

--- app/src/main/java/edu/northeastern/numad22fa_team15/firebaseStickerHistoryRecyclerUtil/StickerHistoryAdapter.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/firebaseStickerHistoryRecyclerUtil/StickerHistoryAdapter.java
@@ -1,20 +1,27 @@
 package edu.northeastern.numad22fa_team15.firebaseStickerHistoryRecyclerUtil;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Date;
 import java.util.List;
 
 import edu.northeastern.numad22fa_team15.R;
+import edu.northeastern.numad22fa_team15.firebaseSendStickerRecyclerUtil.StickerAdapter;
 import edu.northeastern.numad22fa_team15.model.StickerRecord;
 
 public class StickerHistoryAdapter  extends RecyclerView.Adapter<StickerHistoryViewHolder> {
+
+    private static final String TAG = "StickerHistoryAdapter_________";
 
     private List<StickerRecord> results;
     private final Context context;
@@ -33,16 +40,46 @@ public class StickerHistoryAdapter  extends RecyclerView.Adapter<StickerHistoryV
 
     @Override
     public void onBindViewHolder(@NonNull StickerHistoryViewHolder holder, int position) {
-        // TODO: Need to consider the edge case that different versions of the app may contain different stickers.
+        // Consider the edge case that different versions of the app may contain different stickers.
+        try {
+            holder.itemStickerImage.setImageResource(results.get(position).getStickerResourceID());
+        } catch (Exception e) {
+            // If setImageResource operation fails, set the image to a "sticker not available" image
+            Log.v(TAG, "setImageResource operation failed. ");
+            holder.itemStickerImage.setImageResource(R.mipmap.ic_launcher);
+            holder.itemStickerImage.setOnClickListener(view -> {
+                // Open the dialog that tells user the potential reason why a sticker is not available
+                openUnavailableStickerDialog(holder.itemView);
+            });
+        }
 
-        holder.itemStickerImage.setImageResource(results.get(position).getStickerResourceID());
         holder.itemStickerID.setText(String.valueOf(results.get(position).getStickerResourceID()));
         holder.itemSenderUsername.setText(results.get(position).getSender());
         // Convert timestamp long value to Date to String
         long timestampLong = results.get(position).getTimestamp();
         String timestamp = new Date(timestampLong).toString();
-        System.out.println(timestamp);
         holder.itemTimestamp.setText(timestamp);
+    }
+
+    /**
+     * Open a dialog that notifies the user the potential reason of why the sticker is unavailable.
+     * @param view view
+     */
+    private void openUnavailableStickerDialog(View view) {
+        AlertDialog.Builder b = new AlertDialog.Builder(view.getContext());
+        b.setCancelable(false);
+        b.setTitle("Unavailable Sticker");
+        String message = "Potential reason:\nYou may not have the latest version of app. " +
+                "Consider updating your app. Thank you.";
+        b.setMessage(message);
+        b.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = b.create();
+        alertDialog.show();
     }
 
     /**
