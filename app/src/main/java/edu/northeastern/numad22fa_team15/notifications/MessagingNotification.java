13b83446fcb793eb1bb1d1f4b8d6fe6166a8875d
jaimesi <si.ja@northeastern.edu>
2022-11-04 22:08:15 -0400

    Basic notification created

--- app/src/main/java/edu/northeastern/numad22fa_team15/notifications/MessagingNotification.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/notifications/MessagingNotification.java
@@ -1,6 +1,8 @@
 package edu.northeastern.numad22fa_team15.notifications;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
@@ -25,6 +27,11 @@ public class MessagingNotification extends Application {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
+            int important = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
     }
 }
