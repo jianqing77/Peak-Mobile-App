21d3f5a7faa822107e6ee9c0ea1924a505ee98fc
xinyu-hou <61299181+xinyu-hou@users.noreply.github.com>
2022-11-21 16:01:28 -0500

    User Sign Up, Login, Reset Password Functions Implemented

--- app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/PeakFirstPage.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/PeakFirstPage.java
@@ -1,11 +1,23 @@
 package edu.northeastern.numad22fa_team15.activities.peakActivities;
 
+import static edu.northeastern.numad22fa_team15.utils.CommonUtils.closeKeyboard;
+import static edu.northeastern.numad22fa_team15.utils.CommonUtils.displayMessageInSnackbar;
+import static edu.northeastern.numad22fa_team15.utils.CommonUtils.fourDigitPasscodeChecker;
+import static edu.northeastern.numad22fa_team15.utils.CommonUtils.nullOrEmptyInputChecker;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
 import edu.northeastern.numad22fa_team15.R;
+import edu.northeastern.numad22fa_team15.utils.DBHelper;
+import edu.northeastern.numad22fa_team15.utils.IDBHelper;
 
 /**
  * TODO: This activity is just a dummy activity to test out the signup, login, and reset password functionalities.
@@ -13,13 +25,89 @@ import edu.northeastern.numad22fa_team15.R;
 public class PeakFirstPage extends AppCompatActivity {
 
     private static final String TAG = "PeakFirstPage___";
+    private TextInputEditText usernameTextInputEditText;
+    private TextInputEditText firstNameTextInputEditText;
+    private TextInputEditText lastNameTextInputEditText;
+    private TextInputEditText passcodeTextInputEditText;
+
+    private IDBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.v(TAG, "OnCreate()");
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about_team);
+        setContentView(R.layout.activity_test_peak_first_page);
+
+        usernameTextInputEditText = (TextInputEditText) findViewById(R.id.username_text_input_edit_text);
+        firstNameTextInputEditText = (TextInputEditText) findViewById(R.id.first_name_text_input_edit_text);
+        lastNameTextInputEditText = (TextInputEditText) findViewById(R.id.last_name_text_input_edit_text);
+        passcodeTextInputEditText = (TextInputEditText) findViewById(R.id.passcode_text_input_edit_text);
+
+        dbHelper = new DBHelper(PeakFirstPage.this);
+    }
+
+    public void userSignUp(View view) {
+        closeKeyboard(this.getApplicationContext(), view);
+        String username = usernameTextInputEditText.getText().toString();
+        String firstName = firstNameTextInputEditText.getText().toString();
+        String lastName = lastNameTextInputEditText.getText().toString();
+        String passcode = passcodeTextInputEditText.getText().toString();
+        if (nullOrEmptyInputChecker(username, firstName, lastName, passcode)) {
+            String message = "Username, first name, last name, and passcode inputs are all required.";
+            displayMessageInSnackbar(view, message, Snackbar.LENGTH_SHORT);
+            return; // Terminate the method
+        }
+        if (!fourDigitPasscodeChecker(passcode)) {
+            String message = "Passcode needs to have 4 digits.";
+            displayMessageInSnackbar(view, message, Snackbar.LENGTH_SHORT);
+            return;
+        }
+        boolean addResult = dbHelper.addUserTableUser(username, firstName, lastName, passcode);
+        String resultMessage = "Failed to add user"; // Default message
+        if (addResult) {
+            resultMessage = "User was added successfully.";
+        }
+        displayMessageInSnackbar(view, resultMessage, Snackbar.LENGTH_SHORT);
+    }
+
+    public void userLogin(View view) {
+        closeKeyboard(this.getApplicationContext(), view);
+        String username = usernameTextInputEditText.getText().toString();
+        String passcode = passcodeTextInputEditText.getText().toString();
+        if (nullOrEmptyInputChecker(username, passcode)) {
+            String message = "Username and passcode inputs are required.";
+            displayMessageInSnackbar(view, message, Snackbar.LENGTH_SHORT);
+            return;
+        }
+        boolean confirmResult = dbHelper.confirmUserTableUser(username, passcode);
+        String resultMessage = "Login failed.";
+        if (confirmResult) {
+            resultMessage = "Login succeeded.";
+        }
+        displayMessageInSnackbar(view, resultMessage, Snackbar.LENGTH_SHORT);
+    }
+
+    public void userUpdate(View view) {
+        closeKeyboard(this.getApplicationContext(), view);
+        String username = usernameTextInputEditText.getText().toString();
+        String passcode = passcodeTextInputEditText.getText().toString();
+        if (nullOrEmptyInputChecker(username, passcode)) {
+            String message = "Username and passcode inputs are required.";
+            displayMessageInSnackbar(view, message, Snackbar.LENGTH_SHORT);
+            return;
+        }
+        if (!fourDigitPasscodeChecker(passcode)) {
+            String message = "Passcode needs to have 4 digits.";
+            displayMessageInSnackbar(view, message, Snackbar.LENGTH_SHORT);
+            return;
+        }
+        boolean updateResult = dbHelper.updateUserPasswordTableUser(username, passcode);
+        String resultMessage = "Failed to update passcode.";
+        if (updateResult) {
+            resultMessage = "Passcode was updated.";
+        }
+        displayMessageInSnackbar(view, resultMessage, Snackbar.LENGTH_SHORT);
     }
 
     @Override
--- app/src/main/java/edu/northeastern/numad22fa_team15/utils/CommonUtils.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/utils/CommonUtils.java
@@ -29,4 +29,27 @@ public class CommonUtils {
         imm.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 
+    /**
+     * This method checks if any of the inputs is null or empty.
+     * @param inputs a list of input strings
+     * @return true if any put is null or empty. Otherwise, return false
+     */
+    public static boolean nullOrEmptyInputChecker(String... inputs) {
+        for (String input : inputs) {
+            if (input == null || input.isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * This method checks whether the passcode input is a 4 digit number.
+     * @param passcodeInput passcode input
+     * @return true if the passcode is a 4 digit number. Otherwise, return false
+     */
+    public static boolean fourDigitPasscodeChecker(@NonNull String passcodeInput) {
+        return passcodeInput.length() == 4;
+    }
+
 }
