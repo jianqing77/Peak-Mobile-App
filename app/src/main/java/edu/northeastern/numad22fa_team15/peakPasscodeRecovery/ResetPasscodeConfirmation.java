453af579a2aaf30711147513fb07b9262210a46e
jaimesi <si.ja@northeastern.edu>
2022-11-21 16:35:15 -0500

    Added Reset Passcode Confirmation layout

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/peakPasscodeRecovery/ResetPasscodeConfirmation.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team15.peakPasscodeRecovery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class ResetPasscodeConfirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reset_passcode_confirmation);
+    }
+}
