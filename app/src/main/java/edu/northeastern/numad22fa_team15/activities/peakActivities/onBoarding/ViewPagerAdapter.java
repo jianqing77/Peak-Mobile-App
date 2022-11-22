c502bd1d3d13e00fcc78da8dc06067b810670643
Jianiqng Ma <jianqing997@gmail.com>
2022-11-22 17:16:04 -0500

    Add onboarding_slider_layout.xml and matched adapter for the image slider view

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/onBoarding/ViewPagerAdapter.java
@@ -0,0 +1,80 @@
+package edu.northeastern.numad22fa_team15.activities.peakActivities.onBoarding;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import org.w3c.dom.Text;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Context context;
+    int images[] = {
+            R.drawable.ic_onboarding_1,
+            R.drawable.ic_onboarding_2,
+            R.drawable.ic_onboarding_3,
+            R.drawable.ic_onboarding_4
+    };
+
+    int headings[] = {
+            R.string.heading_one,
+            R.string.heading_two,
+            R.string.heading_three,
+            R.string.heading_four
+    };
+
+    int descption[] = {
+            R.string.desc_one,
+            R.string.desc_two,
+            R.string.desc_three,
+            R.string.desc_four,
+
+    };
+
+    public ViewPagerAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.onboarding_slider_layout, container, false);
+
+        ImageView slideTitleImage = (ImageView) view.findViewById(R.id.onboarding_image);
+        TextView slideHeading =(TextView) view.findViewById(R.id.tv_onboarding_header);
+        TextView slideDescription = (TextView) view.findViewById(R.id.tv_onboarding_desc);
+
+        slideTitleImage.setImageResource(images[position]);
+        slideHeading.setText(headings[position]);
+        slideDescription.setText(descption[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
