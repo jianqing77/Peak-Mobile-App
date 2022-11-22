2944eac99afd33995a8a5939c6b8dfc9c35afda5
jaimesi <si.ja@northeastern.edu>
2022-11-21 20:58:55 -0500

    Added Edit Profile layout

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/peakProfile/EditProfile.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team15.peakProfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_profile);
+    }
+}
