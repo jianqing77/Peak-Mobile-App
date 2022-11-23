20b9744e60a32a09ee79e1b25d5e7ea5274403e6
Jianiqng Ma <jianqing997@gmail.com>
2022-11-23 15:26:24 -0500

    Add LockScreen Activity for testing

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/lockScreen/PeakLockScreen.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team15.activities.peakActivities.lockScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class PeakLockScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peak_lock_screen);
+    }
+}
