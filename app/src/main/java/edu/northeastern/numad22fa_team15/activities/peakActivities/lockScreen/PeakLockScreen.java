e4fae66a4663675ffbc09fd728ee8d313640ab3e
jaimesi <si.ja@northeastern.edu>
2022-11-24 18:59:12 -0500

    Creating lock screen layout -- simpler version for now

--- app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/lockScreen/PeakLockScreen.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/lockScreen/PeakLockScreen.java
@@ -2,6 +2,7 @@ package edu.northeastern.numad22fa_team15.activities.peakActivities.lockScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import edu.northeastern.numad22fa_team15.R;
@@ -12,5 +13,7 @@ public class PeakLockScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peak_lock_screen);
+        // Set lock screen to portrait orientation
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
