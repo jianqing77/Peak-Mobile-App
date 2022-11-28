5f38053ee7c77e7f1dcb4979cc77c24bbd2bb293
xinyu-hou <61299181+xinyu-hou@users.noreply.github.com>
2022-11-28 17:38:22 -0500

    Add BottomSheetDialog to EditProfileActivity

--- app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/profilePage/EditProfileActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/profilePage/EditProfileActivity.java
@@ -2,17 +2,21 @@ package edu.northeastern.numad22fa_team15.activities.peakActivities.profilePage;
 
 import static edu.northeastern.numad22fa_team15.utils.CommonUtils.setProfilePictureToGivenImageView;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import edu.northeastern.numad22fa_team15.R;
+import edu.northeastern.numad22fa_team15.activities.firebaseActivities.FirebaseFriendListActivity;
 import edu.northeastern.numad22fa_team15.models.databaseModels.UserModel;
 import edu.northeastern.numad22fa_team15.utils.DBHelper;
 import edu.northeastern.numad22fa_team15.utils.IDBHelper;
@@ -22,6 +26,7 @@ public class EditProfileActivity extends AppCompatActivity {
     private static final String TAG = "EditProfileActivity______";
 
     private ImageView profilePictureImageView;
+    private ImageView editProfileImageView;
     private TextView firstNameTextView;
     private TextView lastNameTextView;
     private TextView usernameTextView;
@@ -38,6 +43,7 @@ public class EditProfileActivity extends AppCompatActivity {
         dbHelper = new DBHelper(EditProfileActivity.this);
 
         profilePictureImageView = (ImageView) findViewById(R.id.profile_picture);
+        editProfileImageView = (ImageView) findViewById(R.id.btn_edit_profile_picture);
         firstNameTextView = (TextView) findViewById(R.id.et_edit_first_name);
         lastNameTextView = (TextView) findViewById(R.id.et_edit_last_name);
         usernameTextView = (TextView) findViewById(R.id.et_edit_username);
@@ -45,8 +51,25 @@ public class EditProfileActivity extends AppCompatActivity {
         // Retrieve user info from database and set the hints
         retrieveUserInfoFromDatabase();
 
-        // TODO: Add onClickListener to profile picture image view.
+        // TODO: Add onClickListener to edit profile picture image view.
+        editProfileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open a dialog that uses change_profile_picture_bottomsheet
+                // REF: https://www.section.io/engineering-education/bottom-sheet-dialogs-using-android-studio/
+                showBottomSheetDialog();
+            }
+        });
+    }
 
+    private void showBottomSheetDialog() {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.change_profile_picture_bottomsheet);
+
+        // TODO: Implement button functions.
+        ImageButton takePictureButton = (ImageButton) bottomSheetDialog.findViewById(R.id.btn_take_profile_picture);
+
+        bottomSheetDialog.show();
     }
 
     private void retrieveUserInfoFromDatabase() {
