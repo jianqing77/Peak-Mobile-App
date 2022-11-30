61866398e1b56e4a394b93c5bee375e9dce60aaa
jaimesi <si.ja@northeastern.edu>
2022-11-30 13:00:37 -0500

    Adding dining seekbar to edit budget page layout

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/PeakCreateBudget.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad22fa_team15.activities.peakActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeakCreateBudget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_budget);
+    }
+}
