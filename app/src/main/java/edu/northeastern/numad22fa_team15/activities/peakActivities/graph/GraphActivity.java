f278e427fc34753121a950492096cba81b92a2b9
Jianiqng Ma <jianqing997@gmail.com>
2022-12-02 19:17:06 -0500

    Add graph and savings activity

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/graph/GraphActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team15.activities.peakActivities.graph;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class GraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
+}
---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/piggySavings/SavingsActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team15.activities.peakActivities.piggySavings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class SavingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savings);
+    }
+}
