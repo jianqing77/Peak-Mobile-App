11fe8b79dc1e7dcac2921eae439be8941e204d44
Jianiqng Ma <jianqing997@gmail.com>
2022-12-02 21:19:42 -0500

    Modify add transaction -- link with new AddTransactionActivity.java

---
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/addTransaction/AddTransactionActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team15.activities.peakActivities.addTransaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad22fa_team15.R;
+
+public class AddTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
