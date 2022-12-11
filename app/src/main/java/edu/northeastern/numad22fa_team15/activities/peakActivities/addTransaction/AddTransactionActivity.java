ca59cb0e1380e3fa860905a4430f30e1a7a695c7
Jianiqng Ma <jianqing997@gmail.com>
2022-12-10 22:40:37 -0500

    Modify pic configuration -- compress photos

--- app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/addTransaction/AddTransactionActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/addTransaction/AddTransactionActivity.java
@@ -254,7 +254,7 @@ public class AddTransactionActivity extends AppCompatActivity implements Adapter
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_PERMISSION_CODE){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                ImagePicker.with(this).crop(9f, 16f).start();
+                ImagePicker.with(this).crop(9f, 16f).compress(1024).start();
             } else {
                 String msg = "Camera permission denied, please allow permission first before accessing the camera features";
                 Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
