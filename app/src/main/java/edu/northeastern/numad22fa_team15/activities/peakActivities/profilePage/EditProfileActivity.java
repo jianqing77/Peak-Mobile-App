0f189887d9021e1b9482c337efd92f739b8ca0cf
Jianiqng Ma <jianqing997@gmail.com>
2022-12-10 22:25:47 -0500

    Modify pic configuration -- profile square crop

--- app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/profilePage/EditProfileActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team15/activities/peakActivities/profilePage/EditProfileActivity.java
@@ -105,7 +105,7 @@ public class EditProfileActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(EditProfileActivity.this,
                     new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
         } else {
-            ImagePicker.with(this).crop().start();
+            ImagePicker.with(this).cropSquare().compress(1024).start();
         }
     }
 
@@ -115,7 +115,7 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_PERMISSION_CODE){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                ImagePicker.with(this).crop().start();
+                ImagePicker.with(this).cropSquare().compress(1024).start();
             } else {
                 String msg = "Camera permission denied, please allow permission first before accessing the camera features";
                 Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
